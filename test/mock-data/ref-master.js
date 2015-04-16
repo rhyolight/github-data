2dd3457f6f2b993205c98c425b745e8ac4f5c609
Matthew Taylor <matt@numenta.org>
2015-04-15 21:37:12 -0700

    Filling tests out for GitData. Created Branch object.

---
+++ test/mock-data/ref-master.js
@@ -0,0 +1,9 @@
+module.exports = {
+    ref: "refs/heads/master",
+        url: "https://api.github.com/repos/rhyolight/sprinter.js/git/refs/heads/master",
+    object: {
+        sha: "6b8cd6ed85a41d407787090c74a76efb981d13e0",
+        type: "commit",
+        url: "https://api.github.com/repos/rhyolight/sprinter.js/git/commits/6b8cd6ed85a41d407787090c74a76efb981d13e0"
+    }
+};
--- test/test-gitdata.js
+++ test/test-gitdata.js
@@ -1,8 +1,9 @@
 var _ = require('lodash')
   , assert = require('chai').assert
   , expect = require('chai').expect
-  , should = require('chai').should()
   , proxyquire = require('proxyquire')
+  , Branch = require('../lib/branch')
+  , mockRefMaster = require('./mock-data/ref-master')
   ;
 
 describe('github-data', function() {
@@ -57,12 +58,63 @@ describe('github-data', function() {
         });
 
         describe('with required configuration', function() {
-
             it('authenticates through GitHub', function() {
                 new GitData('my-username', 'my-password', 'organization', 'repository');
                 assert.ok(authenticated, 'Did not authenticate upon construction.');
             });
+        });
 
+    });
+
+    describe('when getting branch', function() {
+        var mockGitHubInstance = {
+            authenticate: function() {},
+            gitdata: {
+                getReference: function(params, callback) {
+                    expect(params.user).to.equal('my-organization');
+                    expect(params.repo).to.equal('my-repository');
+                    if (params.ref !== 'heads/master') {
+                        return callback({"code":404,"message":"{\"message\":\"Not Found\",\"documentation_url\":\"https://developer.github.com/v3\"}"})
+                    }
+                    callback();
+                }
+            }
+        };
+
+        var GitData = proxyquire('../lib/gitdata', {
+            'github': function () {
+                return mockGitHubInstance;
+            }
+        });
+
+        var gd = new GitData(
+            'my-username', 'my-password', 'my-organization', 'my-repository'
+        );
+
+        it('calls gitdata.getReference() correctly', function(done) {
+            gd.getBranch('master', function() {
+                done();
+            });
+        });
+
+        it('returns proper error when bad branch name', function(done) {
+            gd.getBranch('noop', function(error) {
+                assert.ok(error, "Nonexistant branch name should throw error");
+                expect(error).to.be.instanceOf(Error);
+                expect(error).to.have.property('message');
+                expect(error.message).to.equal('Branch "noop" does not exist.');
+                expect(error).to.have.property('code');
+                expect(error.code).to.equal(404);
+                done();
+            });
+        });
+
+        it('returns a Branch object', function(done) {
+            gd.getBranch('master', function(error, branch) {
+                assert.notOk(error);
+                expect(branch).to.be.instanceOf(Branch);
+                done();
+            });
         });
 
     });
